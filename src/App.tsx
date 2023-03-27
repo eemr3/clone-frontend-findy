214380f17076b8ae948f1ddb16f4bd558c68ae3d
Lucas <mariolucas.2000@outlook.com>
2023-03-27 19:00:26 -0300

    :wrench: chore: Correcoes

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { AppRouter } from "./routers/router.jsx";
+
+function App() {
+
+  return (
+    <Router>
+      <AppRouter />
+    </Router>
+  )
+}
+
+export default App
