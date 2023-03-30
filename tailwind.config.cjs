c1888eeaca4c534244b02a14973b3daa8839b9db
DarcioCarvalho <darcio.nuno@gmail.com>
2023-03-30 20:13:01 -0300

    fix: fixed tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,22 +12,12 @@ module.exports = {
       'green-medium': '#01A195',
       'green-dark': '#015659',
 
-      'blue-dark': '#252C43',
-
       'grey-#1': '#1F1F1F',
       'grey-#2': '#828282',
       'grey-#3': '#E0E0E0',
       'grey-#4': '#F9F9F9',
       'grey-#5': '#FFFFFF',
 
-      'blue': '#326BFF',
-      'red': '#C32323',
-    },
-    boxShadow: {
-      'shadow-#1': '0px 2px 5px 0px #01A1953D',
-      'shadow-#2': '0px 5px 20px 5px #01A1953D',
-      'shadow-#3': '0px 10px 30px 5px #01A1953D',
-      'shadow-button': '0px 1.4526855945587158px 3.6317138671875px 0px #0000001A',
       'tag-blue': '#5EC5FF',
       'tag-green': '#6EFF6B',
       'tag-pink': '#FF4DC2',
@@ -46,9 +36,7 @@ module.exports = {
       'shadow-#3': '0rem 1rem 3rem 0.5rem #01A1953D',
       'shadow-button': '0rem 0.14526855945587158rem 0.36317138671875rem 0rem #0000001A',
       'shadow-tag': ' 0rem 0.17773873805999756rem 0.44434685707092285rem 0rem #0000001A'
-
     },
-
     fontFamily: {
       sans: ['roboto flex', 'sans-serif']
     },
