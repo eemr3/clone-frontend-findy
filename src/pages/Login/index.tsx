83ac9fcfd1fd144aa17f4a84b0fecf3b10fa4f12
Lucas <mariolucas.2000@outlook.com>
2023-04-13 14:37:48 -0300

    :boom: fix: Correction responsive login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -55,20 +55,20 @@ export function Login() {
           className="absolute right-[0]  top-[0] h-[100%] w-[54.6rem] object-cover xl:hidden "
         />
 
-        <div className="flex w-[100%]  max-w-[63.5rem] flex-col items-center rounded-[2.6rem] bg-[#FFFFFF] pb-[4rem] ">
-          <h2 className="mb-[6.4rem] mt-[6.4rem] text-[4.8rem] font-[700] md:text-[4rem] mbl:text-[3.2rem]">
+        <div className="flex w-[100%]  max-w-[63.5rem] flex-col items-center rounded-[2.6rem] bg-[#FFFFFF] pb-[4rem]  ">
+          <h2 className="mb-[6.4rem] mt-[6.4rem] text-[4.8rem] font-[700] md:text-[4rem] mbl:text-[3.2rem] mbl:text-[1.7rem] mbl:mb-[2.8rem]">
             Acesse a sua Conta
           </h2>
 
-          <div className="w-[70%] mbl:w-[85%]  ">
+          <div className="w-[70%] mbl:w-[85%] mbl:flex sm:justify-center">
             <input
               type="email"
               placeholder="insira seu email"
               {...register("email")}
               className={
                 errors.email
-                  ? "h-[6rem] w-[100%] rounded-[0.8rem] border border-red pl-[1rem] text-[2.4rem] placeholder-red"
-                  : "mb-[2.4rem] h-[6rem] w-[100%] rounded-[0.8rem] border border-black pl-[1rem] text-[2.4rem]"
+                  ? "h-[6rem] w-[100%] rounded-[0.8rem] border border-red pl-[1rem] text-[2.4rem] placeholder-red mbl:h-[4.5rem] mbl:text-[1.3rem] mbl:w-[90%] "
+                  : "mb-[2.4rem] h-[6rem] w-[100%] rounded-[0.8rem] border border-black pl-[1rem] text-[2.4rem] mbl:h-[4.5rem]  mbl:text-[1.3rem] mbl:mb-[1.5rem] mbl:w-[90%] "
               }
             />
             <span className=" mb-[1rem] mt-[0.8rem] block  pl-[1rem] text-[1.8rem] text-red">
@@ -80,15 +80,15 @@ export function Login() {
             </span>
           </div>
 
-          <div className="w-[70%]  mbl:w-[85%] ">
+          <div className="w-[70%]  mbl:w-[85%]  mbl:flex mbl:justify-center">
             <input
               type="password"
               placeholder="Confirme sua senha"
               {...register("password")}
               className={
                 errors.password
-                  ? "h-[6rem] w-[100%] rounded-[0.8rem] border border-red pl-[1rem] text-[2.4rem] placeholder-red"
-                  : "mb-[2.4rem] h-[6rem] w-[100%] rounded-[0.8rem] border border-black pl-[1rem] text-[2.4rem]"
+                  ? "h-[6rem] w-[100%] rounded-[0.8rem] border border-red pl-[1rem] text-[2.4rem] placeholder-red mbl:h-[4.5rem] mbl:text-[1.3rem] mbl:w-[90%] "
+                  : "mb-[2.4rem] h-[6rem] w-[100%] rounded-[0.8rem] border border-black pl-[1rem] text-[2.4rem] mbl:h-[4.5rem] mbl:text-[1.3rem] mbl:w-[90%] "
               }
             />
             <span className=" mb-[1rem] mt-[0.8rem] block  pl-[1rem] text-[1.8rem] text-red">
@@ -96,19 +96,19 @@ export function Login() {
             </span>
           </div>
 
-          <div className="mt-[2rem] flex w-[70%] justify-between mbl:w-[80%] ">
+          <div className="mt-[2rem] flex w-[70%] justify-between mbl:w-[80%] mbl:mt-[0]">
             <div className="flex">
               <input
-                className="mr-[1.2rem] h-[2.9rem] w-[2.8rem] border-green-medium"
+                className="mr-[1.2rem] h-[2.5rem] w-[2.8rem] border-green-medium mbl:h-[2.2rem] mbl:mr-[0.5rem] "
                 type="checkbox"
               />
-              <p className="text-[1.6rem]  mbl:text-[1.3rem]">
+              <p className="text-[1.6rem]  mbl:text-[1.3rem] mbl:text-[1rem]">
                 Matenha-me logado
               </p>
             </div>
             <Link
               to="#"
-              className="text-[1.6rem] text-green-medium  mbl:text-[1.3rem] "
+              className="text-[1.6rem] text-green-medium  mbl:text-[1.3rem] mbl:text-[1rem] "
             >
               Esqueceu a senha?
             </Link>
@@ -117,9 +117,9 @@ export function Login() {
             className="mt-[6.6rem] h-[6rem] w-[70%] rounded-[3.2rem] bg-[#01A195] mbl:h-[4rem]  mbl:max-w-[100%]"
             onClick={handleSubmit(onSubmit)}
           >
-            <p className="text-[2.4rem] text-[#FFFFFF]  ">Logar</p>
+            <p className="text-[2.4rem] text-[#FFFFFF] mbl:text-[2.2rem] ">Logar</p>
           </button>
-          <p className="mt-[6.4rem] text-[2.4rem]  mbl:text-[1.6rem]">
+          <p className="mt-[6.4rem] text-[2.4rem]  mbl:text-[1.4rem] mbl:mt-[4.4rem]">
             Você é novo na Findy?{" "}
             <Link to="#" className="text-[#01A195]">
               {" "}
