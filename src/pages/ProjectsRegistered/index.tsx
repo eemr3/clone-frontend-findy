fb1bf6d879b334674e7b6c1ad21be3693671b95a
Emerson Moreira <eemr3@yahoo.com.br>
2023-05-08 12:53:21 -0300

    :recycle: refactor:  redirect with target=_blank property

--- src/pages/ProjectsRegistered/index.tsx
+++ src/pages/ProjectsRegistered/index.tsx
@@ -1,12 +1,12 @@
-import jwt_decode from "jwt-decode";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { Button } from "../../components/Button";
-import { Header } from "../../components/Header";
-import { Heading } from "../../components/Heading";
-import { Tag } from "../../components/Tag";
-import { Text } from "../../components/Text";
-import { getLanguagesById, getProjects } from "../../services/api";
+import jwt_decode from 'jwt-decode';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Heading } from '../../components/Heading';
+import { Tag } from '../../components/Tag';
+import { Text } from '../../components/Text';
+import { getLanguagesById, getProjects } from '../../services/api';
 
 interface Projects {
   id: number;
@@ -20,7 +20,7 @@ interface Projects {
 export function ProjectRegistred() {
   // Teste de Error no nome e e-mail
   const [projects, setProjects] = useState<any[]>([]);
-  const [nameUser, setNameUser] = useState("");
+  const [nameUser, setNameUser] = useState('');
   const [languageNames, setlanguageNames] = useState<any[]>([]);
   const [count, setCount] = useState<number>(100);
   const [indexCount, setIndex] = useState<number>(0);
@@ -28,23 +28,22 @@ export function ProjectRegistred() {
   useEffect(() => {
     async function fetchData() {
       const response = await getProjects();
-      console.log(response);
 
       setProjects(response.data);
       const languages = await Promise.all(
         response.data.map(async (project: any) => {
           const languageIds = project.language;
           const languagePromises = languageIds.map((stackId: any) =>
-            getLanguagesById(stackId?.stackId)
+            getLanguagesById(stackId?.stackId),
           );
           const languageNames = await Promise.all(languagePromises);
           return languageNames;
-        })
+        }),
       );
 
       setlanguageNames(languages);
     }
-    const token: string | any = localStorage.getItem("token");
+    const token: string | any = localStorage.getItem('token');
     const { name }: any = jwt_decode(token);
     const token2: any = jwt_decode(token);
     setNameUser(name);
@@ -92,21 +91,21 @@ export function ProjectRegistred() {
       <article className="flex min-h-[max-context] flex-col items-center justify-center bg-blue-dark pl-[15.9rem] pt-[6.414rem] text-grey-#5 lg:px-[3rem] mbl:justify-center mbl:pb-[5rem]">
         <Heading
           type="lg-leading58"
-          className="text-[5.5rem] leading-[5rem] xl:text-[5rem] lg:text-[4rem] mbl:text-[2.2rem] "
+          className="text-[5.5rem] leading-[5rem] lg:text-[4rem] xl:text-[5rem] mbl:text-[2.2rem] "
         >
           Bem vindo(a) à Findy, ({nameUser.toUpperCase()})!
         </Heading>
 
         <Heading
           type="lg-leading58"
-          className="mt-[8rem] text-[4.5rem]  leading-[6rem] xl:text-[4rem]  lg:text-[2.5rem] mbl:text-[1.8rem]"
+          className="mt-[8rem] text-[4.5rem]  leading-[6rem] lg:text-[2.5rem]  xl:text-[4rem] mbl:text-[1.8rem]"
         >
           Projetos cadastrados
         </Heading>
 
         <Text
           type="md"
-          className=" inline-block xl:text-[2rem] lg:text-[1.5rem] mbl:text-[1.4rem] "
+          className=" inline-block lg:text-[1.5rem] xl:text-[2rem] mbl:text-[1.4rem] "
         >
           Visualize os projetos disponíveis na Findy.
         </Text>
@@ -119,46 +118,55 @@ export function ProjectRegistred() {
         </button>
       </article>
 
-      <section className=" mbl:  mt-[10.2rem] flex flex-col gap-[3rem] bg-[#FFFFFF] pb-[10rem] pt-[8rem] xl:px-[2rem]  md:px-[2rem]  mbl:px-[1.5rem]  mbl:pt-[2rem] ">
+      <section className=" mbl:  mt-[10.2rem] flex flex-col gap-[3rem] bg-[#FFFFFF] pb-[10rem] pt-[8rem] md:px-[2rem]  xl:px-[2rem]  mbl:px-[1.5rem]  mbl:pt-[2rem] ">
         <div className="mx-auto flex h-[100%] w-[100%] flex-col items-center  gap-[3rem] ">
           {projects?.map((project: Projects, index) => {
             return (
               <div
-                key={index}
-                className="mx-auto flex h-[100%] h-[100%] w-[100%] max-w-[120rem] items-center shadow-shadow-#1"
+                key={project.id}
+                className="mx-auto flex h-[100%] w-[100%] max-w-[120rem] items-center shadow-shadow-#1"
               >
                 <div className="h-[19.3rem] w-[1.3rem] rounded-br-[0.8rem] rounded-tr-[0.8rem] bg-[#01A195] md:h-[15rem]  md:w-[1rem]  mbl:h-[15rem] mbl:w-[0.8rem]"></div>
                 <div className="w-[100%] pb-[1rem] pl-[11.5rem] pr-[6.7rem] pt-[3rem] font-bold md:pl-[4rem] mbl:pl-[2rem] mbl:pr-[1rem]">
-                  <h3 className="mb-[1.2rem] text-[4.8rem] xl:text-[4rem] lg:text-[1.5rem] lg:text-[2.2rem] mbl:text-[1.6rem]">
+                  <h3 className="mb-[1.2rem] text-[4.8rem] md:text-[1.5rem] lg:text-[2.2rem] xl:text-[4rem] mbl:text-[1.6rem]">
                     {project?.name}
                   </h3>
 
-                  <p className="mb-[3rem] text-[3.2rem] xl:text-[2.5rem] lg:text-[1.5rem] lg:text-[1.7rem]">
-                    {project?.projectScope.substring(0, counts[index] ?? 100) +
-                      "..."}
+                  <p className="mb-[3rem] text-[3.2rem] md:text-[1.5rem] lg:text-[1.7rem] xl:text-[2.5rem]">
+                    {project?.projectScope.substring(0, counts[index] ?? 100) + '...'}
                   </p>
 
                   <div className="mb-[2rem] flex gap-[2rem]">
-                    {languageNames[index]?.map((arra: any) => {
+                    {languageNames[index]?.map((language: any) => {
                       return (
-                        <Tag label={arra?.data?.title} tagColor={"green"}></Tag>
+                        <Tag
+                          key={language?.data?.id}
+                          label={language?.data?.title}
+                          tagColor={'green'}
+                        ></Tag>
                       );
                     })}
                   </div>
                   <div className="mb-[3rem] flex items-end justify-end gap-[4rem]">
-                    <Button className="flex h-[6rem] w-[60%] max-w-[40.6rem] items-center justify-center justify-center rounded-[3.2rem] border border-[#01A195]  bg-[#ffffff] px-[0]  xl:h-[5rem] lg:h-[4rem] lg:w-[48%]   md:h-[3rem]  md:w-[75%] md:max-w-[27rem] mbl:h-[2.5rem] mbl:w-[65%] mbl:max-w-[16rem] ">
+                    <Button
+                      className="flex h-[6rem] w-[60%] max-w-[40.6rem] items-center justify-center 
+                    rounded-[3.2rem] border border-[#01A195]  bg-[#ffffff] px-[0]  md:h-[3rem] md:w-[75%] md:max-w-[27rem]   lg:h-[4rem]  lg:w-[48%] xl:h-[5rem] mbl:h-[2.5rem] mbl:w-[65%] mbl:max-w-[16rem] "
+                    >
                       <Link
                         className="flex h-full w-full items-center justify-center "
                         to={project?.urlTeamSelection}
+                        target="_blank"
                       >
-                        <p className="text-[2.4rem] text-[#01A195] xl:text-[2rem]   lg:text-[1.5rem] md:text-[1.5rem] mbl:text-[0.9rem] ">
+                        <p className="text-[2.4rem] text-[#01A195] md:text-[1.5rem]   lg:text-[1.5rem] xl:text-[2rem] mbl:text-[0.9rem] ">
                           CADASTRE-SE NESSE PROJETO
                         </p>
                       </Link>
                     </Button>
 
-                    <button
-                      className=" h-[6rem] w-[60%] max-w-[15.6rem] rounded-[3.2rem] bg-[#01A195] px-[2.5rem] xl:h-[5rem]  lg:h-[4rem]  lg:w-[45%]   lg:px-[1rem] md:h-[3rem] md:w-[30%] md:max-w-[10rem] mbl:h-[2.5rem] mbl:w-[30%] mbl:px-[0.1rem] "
+                    <div
+                      className="flex h-[6rem] w-[60%] max-w-[15.6rem] flex-col justify-center rounded-[3.2rem] bg-[#01A195] 
+                      px-[2.5rem] md:h-[3rem]  md:w-[30%]  md:max-w-[10rem]   lg:h-[4rem] lg:w-[45%] 
+                      lg:px-[1rem] xl:h-[5rem] mbl:h-[2.5rem] mbl:w-[30%] mbl:px-[0.1rem] "
                       onClick={() => {
                         handleShowMore(index);
                         if (showMoreIndex !== null) {
@@ -166,16 +174,17 @@ export function ProjectRegistred() {
                         }
                       }}
                     >
-                      <p className="text-[2.4rem] text-[#FFFFFF] xl:text-[2rem] lg:text-[1.5rem]  lg:text-[1.5rem] md:text-[1.3rem] mbl:text-[1.1rem]">
+                      <p
+                        className="text-center text-[2rem] text-[#FFFFFF] sm:text-[1.3rem] md:text-[1.5rem] 
+                       lg:text-[1.5rem] xl:text-[2rem] mbl:text-[1.1rem]"
+                      >
                         {showMoreIndex === index ? (
                           <button onClick={handleShowLess}>Ver Menos</button>
                         ) : (
-                          <button onClick={() => handleShowMore(index)}>
-                            Ver Mais
-                          </button>
+                          <button onClick={() => handleShowMore(index)}>Ver Mais</button>
                         )}
                       </p>
-                    </button>
+                    </div>
                   </div>
                 </div>
               </div>
