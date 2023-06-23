a43559d531cfcd6e6641b1a232e0e10461d55ce8
DarcioCarvalho <darcio.nuno@gmail.com>
2023-06-23 10:53:33 -0300

    feat:added the personal data of survey in the localstorage

--- src/pages/Survey/components/HeaderSurvey.tsx
+++ src/pages/Survey/components/HeaderSurvey.tsx
@@ -1,12 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import { Button } from '../../../components/Button';
 
 import { LogoSmall } from '../../../components/LogoSmall';
 
 export const HeaderSurvey = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="w-max-[128rem] mx-[8rem] flex justify-between pt-[3.3rem]">
       <LogoSmall />
-      <Button className="text-[1.4rem] font-semibold normal-case leading-[1.8rem] tracking-[0.091rem]">
+      <Button
+        className="text-[1.4rem] font-semibold normal-case leading-[1.8rem] tracking-[0.091rem]"
+        onClick={() => navigate('/')}
+      >
         Salvar e sair
       </Button>
     </div>
